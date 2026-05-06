bbce8dd315f08040629b6b7abee5e43c51db4fb2
Your Name <shobhitgopalzz@gmail.com>
2026-05-06 22:52:43 +0530

    first check

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/time', function () {
+    return view('time');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
